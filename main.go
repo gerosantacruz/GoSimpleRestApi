2dc9bd04f0a7450f4826f4c901cdde35b070035c
gerosantacruz <geronimo.santacruz2017@gmail.com>
2020-05-02 17:48:28 -0300

    get and post tasks work

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"io/ioutil"
 	"fmt"
 	"net/http"
 	"log"
+	"encoding/json"
 	"github.com/gorilla/mux"
 )
 
@@ -27,11 +29,35 @@ func indexRoute(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w, "Welcome")
 }
 
+func getTasks(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(tasks)
+}
+
+func createNewTask(w http.ResponseWriter, r *http.Request){
+	var newTask task
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprintf(w, "Insert a valid task")
+	}
+
+	json.Unmarshal(reqBody, &newTask)
+	newTask.ID = len(tasks) + 1
+
+	tasks = append(tasks, newTask)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(newTask)
+}
+
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
 
 	//routes
 	router.HandleFunc("/", indexRoute)
+	router.HandleFunc("/tasks", getTasks).Methods("GET")
+	router.HandleFunc("/tasks", createNewTask).Methods("POST")
+	router.HandleFunc("/tasks/{id}", )
 
 	//Init server
 	log.Fatal(http.ListenAndServe(":3000", router))
